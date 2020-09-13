7145d0cbe8a237b40064d1b5fa63a09dd6f53659
zhum <zhumeng@skyvis.com.cn>
2020-09-13 11:54:16 +0800

    1.创建父工程，编写父工程的pom文件 2.创建支付系统子模块，编写pom文件和yml配置文件等

--- cloud-provider-payment8001/src/main/java/cn/com/dmg/springcloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/cn/com/dmg/springcloud/PaymentMain8001.java
@@ -1,13 +1,12 @@
-package cn.com.dmg;
+package cn.com.dmg.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CloudApplication {
-
+public class PaymentMain8001 {
     public static void main(String[] args) {
-        SpringApplication.run(CloudApplication.class, args);
+        SpringApplication.run(PaymentMain8001.class,args);
     }
 
 }
