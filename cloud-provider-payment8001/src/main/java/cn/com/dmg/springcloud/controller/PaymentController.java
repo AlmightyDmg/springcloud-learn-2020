6236b9017609288af6f82f63e6d8861516daddb2
zhum <zhumeng@skyvis.com.cn>
2020-09-19 15:23:59 +0800

    1.服务发现Discovery 2.关闭7001的自我保护，使用8001配合测试7001关闭的自我保护

--- cloud-provider-payment8001/src/main/java/cn/com/dmg/springcloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/cn/com/dmg/springcloud/controller/PaymentController.java
@@ -5,9 +5,12 @@ import cn.com.dmg.springcloud.entities.Payment;
 import cn.com.dmg.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -18,6 +21,9 @@ public class PaymentController {
     //读取application.yml文件中配置的port
     @Value("${server.port}")
     private String serverPort;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @PostMapping(value = "/payment/create")
     //参数这里不加RequestBody注解，在80调用的时候，serial的值不会自动封装到实体
@@ -40,5 +46,20 @@ public class PaymentController {
         } else {
             return new CommonResult(444,"查询失败,查询id为" + id,null);
         }
+    }
+
+    @GetMapping(value = "/payment/discovery")
+    public Object discovery(){
+        //获得Eureka中注册的所有服务名称
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            log.info("**********service:" + service);
+        }
+        //根据服务名称获得该服务下所有的实例
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getInstanceId() + "\t" + instance.getHost() + "\t" + instance.getPort() + "\t" + instance.getUri());
+        }
+        return this.discoveryClient;
     }
 }
