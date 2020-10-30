dc81a1972a4920cdbf271aba025b77ec782e25fb
zhum <zhumeng@skyvis.com.cn>
2020-10-30 22:51:05 +0800

    集成使用Nacos V2.0

--- cloudalibaba-config-nacos-client3377/src/main/java/cn/com/dmg/springcloud/alibaba/controller/ConfigClientController.java
+++ cloudalibaba-config-nacos-client3377/src/main/java/cn/com/dmg/springcloud/alibaba/controller/ConfigClientController.java
@@ -5,8 +5,7 @@ import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
-@RefreshScope
-
+@RefreshScope //支持Nacos的动态刷新
 public class ConfigClientController{
     @Value("${config.info}")
     private String configInfo;
--- cloudalibaba-consumer-nacos-order83/src/main/java/cn/com/dmg/springcloud/alibaba/config/ApplicationContextConfig.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/cn/com/dmg/springcloud/alibaba/config/ApplicationContextConfig.java
@@ -2,8 +2,10 @@ package cn.com.dmg.springcloud.alibaba.config;
 
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration
 public class ApplicationContextConfig {
     @Bean
     @LoadBalanced
