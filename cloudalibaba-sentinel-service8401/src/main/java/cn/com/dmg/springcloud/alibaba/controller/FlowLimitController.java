ef6fcd26817b652b2776c75087ff07b383bdfc33
zhum <zhumeng@skyvis.com.cn>
2020-11-02 17:19:58 +0800

    集成使用sentinel  v1.0

---
+++ cloudalibaba-sentinel-service8401/src/main/java/cn/com/dmg/springcloud/alibaba/controller/FlowLimitController.java
@@ -0,0 +1,17 @@
+package cn.com.dmg.springcloud.alibaba.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FlowLimitController{
+    @GetMapping("/testA")
+    public String testA() {
+        return "------testA";
+    }
+
+    @GetMapping("/testB")
+    public String testB() {
+        return "------testB";
+    }
+}
