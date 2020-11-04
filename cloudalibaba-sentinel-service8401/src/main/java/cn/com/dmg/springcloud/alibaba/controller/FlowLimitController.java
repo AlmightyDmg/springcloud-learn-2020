0d4c2199033bb21f9722ca6cf2be9b1a6262db81
zhum <zhumeng@skyvis.com.cn>
2020-11-04 20:53:33 +0800

    演示sentinel的流控规则

--- cloudalibaba-sentinel-service8401/src/main/java/cn/com/dmg/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/cn/com/dmg/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,8 +1,10 @@
 package cn.com.dmg.springcloud.alibaba.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class FlowLimitController{
     @GetMapping("/testA")
@@ -12,6 +14,7 @@ public class FlowLimitController{
 
     @GetMapping("/testB")
     public String testB() {
+        log.info(Thread.currentThread().getName() + "...testB");
         return "------testB";
     }
 }
