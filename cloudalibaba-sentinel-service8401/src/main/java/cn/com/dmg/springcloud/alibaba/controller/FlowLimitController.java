ce7fac076e2c8966b47bd6c53c88a470b9050154
zhum <zhumeng@skyvis.com.cn>
2020-11-06 21:40:05 +0800

    演示sentinel的降级规则和热点key降级规则

--- cloudalibaba-sentinel-service8401/src/main/java/cn/com/dmg/springcloud/alibaba/controller/FlowLimitController.java
+++ cloudalibaba-sentinel-service8401/src/main/java/cn/com/dmg/springcloud/alibaba/controller/FlowLimitController.java
@@ -1,8 +1,13 @@
 package cn.com.dmg.springcloud.alibaba.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RestController
@@ -16,5 +21,35 @@ public class FlowLimitController{
     public String testB() {
         log.info(Thread.currentThread().getName() + "...testB");
         return "------testB";
+    }
+
+    @GetMapping("/testD")
+    public String testD() {
+        try {
+            TimeUnit.SECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        log.info("testD 测试RT");        return "------testD";
+    }
+
+    @GetMapping("/testE")
+    public String testE(){
+        log.info("testE 测试异常数");
+        int age = 10/0;
+        return "------testE 测试异常数";
+    }
+
+    @GetMapping("/testHotKey")
+    //当前台配置的热点限流  【testHotKey】 不符合限流规范的时候，要跳转到兜底方法【deal_testHotKey】
+    @SentinelResource(value = "testHotKey",blockHandler = "deal_testHotKey")
+    public String testHotKey(@RequestParam(value = "p1",required = false) String p1,
+                             @RequestParam(value = "p2",required = false) String p2) {
+        //int age = 10/0;
+        return "------testHotKey";
+    }
+    // 兜底方法
+    public String deal_testHotKey (String p1, String p2, BlockException exception){
+        return "------deal_testHotKey,o(╥﹏╥)o";
     }
 }
